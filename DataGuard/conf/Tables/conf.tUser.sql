0e3f634bcabc60050a845be03461f9f6dff24f73
Damian Domański <d.domanski@dalion.pl>
2024-03-09 12:02:01 +0100

    Added defaultSchema to tUser

--- DataGuard/conf/Tables/conf.tUser.sql
+++ DataGuard/conf/Tables/conf.tUser.sql
@@ -2,6 +2,7 @@
 (
 	[UserId]			 INT				IDENTITY (1, 1)													NOT NULL,
 	[UserName]			 NVARCHAR(256)																		NOT	NULL,
+	[DefaultSchema]		 NVARCHAR(128)																			NULL,
 	[IsActive]			 BIT														DEFAULT(1)				NOT NULL,
 	[CreatedBy]			 NVARCHAR (128)		CONSTRAINT [conf_dfUser_CreatedBy]		DEFAULT (SUSER_SNAME())	NOT NULL,
 	[CreatedOn]			 DATETIME2(3)		CONSTRAINT [conf_dfUser_CreatedOn]		DEFAULT (SYSDATETIME())	NOT NULL,
