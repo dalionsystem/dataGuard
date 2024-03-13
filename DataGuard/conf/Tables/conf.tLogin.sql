ccca519da742043896ae7dcf6c663e172d696123
Damian Domański <d.domanski@dalion.pl>
2024-03-13 01:27:30 +0100

    Added Indexec to  tables,  Set relation Login to UserId

--- DataGuard/conf/Tables/conf.tLogin.sql
+++ DataGuard/conf/Tables/conf.tLogin.sql
@@ -1,12 +1,12 @@
 ﻿CREATE TABLE [conf].[tLogin]
 (
-	[LoginId]				INT					IDENTITY (1, 1)														NOT NULL,
-	[LoginName]				NVARCHAR(256)																			NOT	NULL,
-	[TypeLoginId]			CHAR(1)														DEFAULT('S')				NOT NULL,
-	[DefaultDatabaseName]	NVARCHAR(128)																				NULL,
-	[IsActive]				BIT															DEFAULT(1)					NOT NULL,
+	[LoginId]				INT				IDENTITY (1, 1)														NOT NULL,
+	[LoginName]				NVARCHAR(256)																		NOT	NULL,
+	[TypeLoginId]			CHAR(1)														DEFAULT('S')			NOT NULL,
+	[DefaultDatabaseName]	NVARCHAR(128)																			NULL,
+	[IsActive]				BIT															DEFAULT(1)				NOT NULL,
 	[CreatedBy]				NVARCHAR (128)	CONSTRAINT [conf_dfLogin_CreatedBy]			DEFAULT (SUSER_SNAME())	NOT NULL,
-	[CreatedOn]				DATETIME2(3)	CONSTRAINT [conf_dfLogin_CreatedOn]			DEFAULT (SYSDATETIME())		NOT NULL,
+	[CreatedOn]				DATETIME2(3)	CONSTRAINT [conf_dfLogin_CreatedOn]			DEFAULT (SYSDATETIME())	NOT NULL,
 	[LastModifiedBy]		NVARCHAR (128)	CONSTRAINT [conf_dfLogin_LastModifiedBy]	DEFAULT (SUSER_SNAME())	NOT NULL,
 	[LastModifiedOn]		DATETIME2(3)	CONSTRAINT [conf_dfLogin_LastModifiedOn]	DEFAULT (SYSDATETIME())	NOT NULL,
 
@@ -14,4 +14,10 @@
 		CONSTRAINT [conf_fkLogin_ToTypeLogin]	FOREIGN KEY ([TypeLoginId]) REFERENCES [def].[tTypeLogin] ([TypeLoginId]),
 )
 
+GO
 
+
+
+CREATE UNIQUE NONCLUSTERED INDEX [uxLogin_LoginName]
+	ON [conf].[tLogin]([LoginName] ASC);
+GO
--- DataGuard/conf/Tables/conf.tUser.sql
+++ DataGuard/conf/Tables/conf.tUser.sql
@@ -2,11 +2,22 @@
 (
 	[UserId]			 INT				IDENTITY (1, 1)													NOT NULL,
 	[UserName]			 NVARCHAR(256)																		NOT	NULL,
+--	[DatabaseId]		 INT																					NULL,
+	[LoginId]			 INT																					NULL,
 	[DefaultSchema]		 NVARCHAR(128)																			NULL,
 	[IsActive]			 BIT														DEFAULT(1)				NOT NULL,
+
 	[CreatedBy]			 NVARCHAR (128)		CONSTRAINT [conf_dfUser_CreatedBy]		DEFAULT (SUSER_SNAME())	NOT NULL,
 	[CreatedOn]			 DATETIME2(3)		CONSTRAINT [conf_dfUser_CreatedOn]		DEFAULT (SYSDATETIME())	NOT NULL,
 	[LastModifiedBy]	 NVARCHAR (128)		CONSTRAINT [conf_dfUser_LastModifiedBy]	DEFAULT (SUSER_SNAME())	NOT NULL,
 	[LastModifiedOn]	 DATETIME2(3)		CONSTRAINT [conf_dfUser_LastModifiedOn]	DEFAULT (SYSDATETIME())	NOT NULL,
-												CONSTRAINT [conf_pkUser]					PRIMARY KEY CLUSTERED ([UserId] ASC)
+											CONSTRAINT [conf_pkUser]				PRIMARY KEY CLUSTERED ([UserId] ASC),
+											CONSTRAINT [conf_fkUser_ToLogin]		FOREIGN KEY ([LoginId]) REFERENCES [conf].[tLogin] ([LoginId]),
 )
+
+GO 
+
+
+CREATE UNIQUE NONCLUSTERED INDEX [uxUser_UserName]
+	ON [conf].[tUser]([UserName] ASC);
+GO
