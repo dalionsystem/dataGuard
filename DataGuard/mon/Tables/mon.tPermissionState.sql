7f4404284f48c1a193e5a6d02868be3002479099
Damian Domański <d.domanski@dalion.pl>
2024-03-14 01:46:08 +0100

    add table mon.tPermissionState

---
+++ DataGuard/mon/Tables/mon.tPermissionState.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [mon].[tPermissionState]
+(
+--	[Id] INT NOT NULL PRIMARY KEY,
+	[Database]		SYSNAME NULL,
+	[User]			SYSNAME NULL,
+	[Role]			SYSNAME NULL,
+	[ObjectType]	SYSNAME NULL,
+	[Schema]		SYSNAME NULL,
+	[ObjectName]	SYSNAME NULL,
+	[Permission]	SYSNAME NULL
+
+)
