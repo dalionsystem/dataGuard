d9219bbfa2ca577ffb7e17b6b4abb10944b216bb
Damian Domański <d.domanski@dalion.pl>
2024-03-16 02:34:34 +0100

    finish proc dbo.pIsSchemaExists

--- DataGuard/dbo/Procedures/dbo.pIsSchemaExists.sql
+++ DataGuard/dbo/Procedures/dbo.pIsSchemaExists.sql
@@ -1,10 +1,26 @@
 ﻿CREATE PROCEDURE [dbo].[pIsSchemaExists]
 	@SchemaName		sysname,
-	@DatabaseName	sysname 
+	@DatabaseName	sysname,
+	@IsDebug		BIT		= 0
 AS
 	DECLARE @Sql nvarchar(3000)
 			,@ErrorMesssage nvarchar(2000) 
 			,@Result BIT = 0
+			,@ExecQuery nvarchar(4000)
+			,@CRLF CHAR(2) = CHAR(13)+CHAR(10)
+			,@Tab nvarchar(10) = CHAR(9)
+
+	IF @IsDebug = 1 
+	BEGIN
+		SET @ExecQuery = CONCAT( 'EXEC ', QUOTENAME(OBJECT_SCHEMA_NAME(@@PROCID)), '.', QUOTENAME(OBJECT_NAME(@@PROCID)), @CRLF,
+								@Tab, '@SchemaName = ',		@SchemaName,	@CRLF,
+								@Tab, '@DatabaseName = ', @DatabaseName,	@CRLF,
+								@Tab, '@IsDebug = ', @IsDebug )
+
+		PRINT @ExecQuery
+		 
+	END
+
 
 	IF DB_ID(@DatabaseName) IS NULL OR HAS_DBACCESS(@DatabaseName) = 0
 	BEGIN
@@ -16,6 +32,10 @@ AS
 	--TODO
 	SET @sql = CONCAT('SELECT @Result = 1 FROM ',QUOTENAME(@DatabaseName),'.[sys].[schemas] where [name] = @SchemaName')
 	PRINT @sql
+
+	EXEC SP_executesql @sql, N'@Result bit OUTPUT, @SchemaName sysname', @Result=@Result OUTPUT, @SchemaName =@SchemaName
+
+	PRINT @Result
 
 --
 
