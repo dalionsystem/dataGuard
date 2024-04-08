44c4fe013264b17a3309d43c0184c2b34d8b0d05
Damian Domański <d.domanski@dalion.pl>
2024-04-09 00:53:01 +0200

    Add proc  dbo.pGetListOfInstanceLogin

--- DataGuard/dbo/Procedures/dbo.pGetListOfInstanceLogins.sql
+++ DataGuard/dbo/Procedures/dbo.pGetListOfInstanceLogins.sql
@@ -1,6 +1,26 @@
 ﻿CREATE PROCEDURE [dbo].[pGetListOfInstanceLogin]
-	@param1 int = 0,
-	@param2 int
+	@IsDebug		BIT		= 0
 AS
-	SELECT @param1, @param2
-RETURN 0
+	DECLARE @Sql nvarchar(3000)
+			,@ErrorMesssage nvarchar(2000) 
+			,@ExecQuery nvarchar(4000)
+			,@CRLF CHAR(2) = CHAR(13)+CHAR(10)
+			,@Tab nvarchar(10) = CHAR(9)
+
+	IF @IsDebug = 1 
+	BEGIN
+		SET @ExecQuery = CONCAT( 'EXEC ', QUOTENAME(OBJECT_SCHEMA_NAME(@@PROCID)), '.', QUOTENAME(OBJECT_NAME(@@PROCID)), @CRLF,
+								@Tab, ',@IsDebug = ', @IsDebug )
+		PRINT @ExecQuery		 
+	END
+
+
+
+		SELECT  'SERVER'		 AS [ClassDesc]
+				,s.[type]		 AS [Type]
+				,s.[name]		 AS [LoginName]	
+		FROM		sys.server_principals (nolock) s
+
+
+
+	EXEC SP_executesql @sql
