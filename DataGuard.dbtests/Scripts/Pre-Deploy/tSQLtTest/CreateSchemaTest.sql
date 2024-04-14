3bd4ea395fc1a7716a1e1b49be4a639182deefcc
Damian Domański <d.domanski@dalion.pl>
2024-04-15 00:31:46 +0200

    Fix Test On schema test for DacPack

--- DataGuard.dbtests/Scripts/Pre-Deploy/tSQLtTest/CreateSchemaTest.sql
+++ DataGuard.dbtests/Scripts/Pre-Deploy/tSQLtTest/CreateSchemaTest.sql
@@ -1,2 +1,8 @@
 ﻿IF SCHEMA_ID('test') IS NULL
 	EXEC tSQLt.NewTestClass 'test';
+GO
+
+EXEC sp_addextendedproperty @name = N'tSQLt.TestClass', 
+                        @value = 1,
+                        @level0type = 'SCHEMA',
+                        @level0name = 'test';
