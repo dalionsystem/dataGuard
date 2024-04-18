af7640c0c2ac6a2d01f16ceebdee7d2f93cc1c94
Damian Domański <d.domanski@dalion.pl>
2024-04-19 00:17:48 +0200

    add test for conf.tDatabase  try change CreatedOn column

---
+++ DataGuard.dbtests/Test/Procedures/test conf.tDatabase try change CreatedOn.sql
@@ -0,0 +1,44 @@
+﻿CREATE PROCEDURE [test].[test conf.tDatabase try change CreatedOn]
+AS
+BEGIN
+
+    DECLARE  @DatabaseName          SYSNAME =  'TestDB'
+
+            ,@DatabaseId            INT
+            ,@IsPermissionActive    BIT
+            ,@CreatedOn        DATETIME2(3)
+            ,@NewLastModifiedOn     DATETIME2(3)
+
+
+    SELECT TOP (1)
+             @DatabaseId = [DatabaseId]    
+            ,@IsPermissionActive = [IsPermissionActive]
+        --  ,[LastModifiedBy]
+            ,@CreatedOn = [LastModifiedOn]
+    FROM [conf].[tDatabase]
+    WHERE [DatabaseName] = @DatabaseName
+
+
+    IF @DatabaseId IS NOT NULL
+    BEGIN 
+
+
+    	EXEC tSQLt.ExpectException @ExpectedMessagePattern = 'Updating columns CreatedBy, CreatedOn is not allowed!', @ExpectedSeverity = NULL, @ExpectedState = NULL;
+  
+        UPDATE [DataGuard].[conf].[tDatabase]
+            SET [CreatedOn] = '2000-01-01 00:00:00'
+        WHERE DatabaseId = @DatabaseId
+
+
+    --    SELECT TOP 1 @NewLastModifiedOn = [LastModifiedOn] FROM [conf].[tDatabase] WHERE [DatabaseId] = @DatabaseId
+
+    --    PRINT CONCAT('@LastModifiedOn=<',@LastModifiedOn,'>, @NewLastModifiedOn=<', @NewLastModifiedOn,'>') 
+    --     EXEC tSQLt.AssertNotEquals @LastModifiedOn, @NewLastModifiedOn;
+
+    END 
+    ELSE 
+          DECLARE @ErrorMessage nvarchar(200) = CONCAT('The database record ', @DatabaseName, ' does not Exists in conf.tDatabase table')
+          EXEC tSQLt.Fail @ErrorMessage 
+  
+END;
+GO
