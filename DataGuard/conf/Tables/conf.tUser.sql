f34275eb71cddbb8f84c1cce3d330a1c55af6913
Damian Domański <d.domanski@dalion.pl>
2024-05-04 00:52:50 +0200

    Add Trigger to conf.tUser

--- DataGuard/conf/Tables/conf.tUser.sql
+++ DataGuard/conf/Tables/conf.tUser.sql
@@ -21,3 +21,94 @@ GO
 CREATE UNIQUE NONCLUSTERED INDEX [uxUser_UserName]
 	ON [conf].[tUser]([UserName] ASC);
 GO
+
+
+CREATE TRIGGER [conf].[trg_tUser_ModyficationMeta]
+	ON [conf].[tUser]
+	WITH EXECUTE AS OWNER		
+	AFTER INSERT, UPDATE 
+AS
+BEGIN 
+	
+	IF (ROWCOUNT_BIG() =0)
+	RETURN;
+
+	SET NOCOUNT ON;
+	IF EXISTS (
+		SELECT CreatedBy, CreatedOn FROM deleted
+		EXCEPT 
+		SELECT CreatedBy, CreatedOn FROM inserted
+	)
+	BEGIN
+		;THROW 51000, N'Updating columns CreatedBy, CreatedOn is not allowed!', 1;
+	END
+
+	DECLARE @SuserSname nvarchar(128) 
+			,@Datetime DATETIME2(3) = SYSDATETIME()
+
+	EXECUTE AS CALLER 
+		SET @SuserSname = SUSER_SNAME()
+	REVERT;
+
+	--CreateRecord
+	UPDATE u 
+		SET CreatedBy		= @SuserSname 
+		   ,CreatedOn		= @Datetime
+		   ,LastModifiedBy	= @SuserSname
+		   ,LastModifiedOn	= @Datetime
+	FROM inserted i
+	INNER JOIN [conf].[tUser] u ON i.UserId = u.UserId
+	LEFT JOIN deleted d				ON i.UserId = d.UserId
+	WHERE d.UserId IS NULL
+
+
+
+	IF EXISTS (
+		SELECT LastModifiedBy, LastModifiedOn FROM deleted
+		EXCEPT 
+		SELECT LastModifiedBy, LastModifiedOn FROM inserted
+	)
+	AND NOT EXISTS (
+		SELECT UserName, LoginId, DefaultSchema, IsActive FROM deleted
+		EXCEPT 
+		SELECT UserName, LoginId, DefaultSchema, IsActive FROM inserted
+	)
+	BEGIN 
+		UPDATE u 
+			SET LastModifiedBy	= @SuserSname
+		FROM inserted i
+		INNER JOIN [conf].[tUser] u ON i.UserId = u.UserId
+		LEFT JOIN deleted d				ON i.UserId = d.UserId
+		WHERE	i.LastModifiedBy	<> @SuserSname
+
+		UPDATE u 
+			SET LastModifiedOn	= d.LastModifiedOn
+		FROM inserted i
+		INNER JOIN [conf].[tUser] u ON i.UserId = u.UserId
+		LEFT JOIN deleted d				ON i.UserId = d.UserId
+		WHERE	i.LastModifiedBy	= @SuserSname
+			AND i.LastModifiedOn	<> @Datetime
+	END
+
+	IF EXISTS (
+		SELECT * FROM deleted
+		EXCEPT 
+		SELECT * FROM inserted
+	)
+	BEGIN 
+	--UpdateRecord
+	UPDATE u 
+		SET LastModifiedBy	= @SuserSname
+		   ,LastModifiedOn	= @Datetime
+	FROM inserted i
+	INNER JOIN [conf].[tUser] u ON i.UserId = u.UserId
+	LEFT JOIN deleted d				ON i.UserId = d.UserId
+	WHERE	i.LastModifiedBy		<> @SuserSname
+		OR  i.LastModifiedOn		<> @Datetime
+	--	OR  i.[LoginId]				<> d.[LoginId]
+	--  OR  i.[DefaultSchema]	<> d.[DefaultSchema]
+	--	OR	i.[IsActive]			<> d.[IsActive]
+
+	END 
+
+END 
