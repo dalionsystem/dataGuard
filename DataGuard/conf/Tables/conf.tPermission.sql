8a89f9c7c33c3e26a84f69a04605e4811b93b6fd
Damian Domański <d.domanski@dalion.pl>
2024-05-05 00:31:57 +0200

    Add Triger to conf.tPermission

--- DataGuard/conf/Tables/conf.tPermission.sql
+++ DataGuard/conf/Tables/conf.tPermission.sql
@@ -32,3 +32,94 @@ ALTER TABLE [conf].[tPermission] ADD CONSTRAINT conf_chkPermission_ObjectType  C
 																					 --'Securable',
 GO
 
+
+
+CREATE TRIGGER [conf].[trg_t_ModyficationMeta]
+	ON [conf].[tPermission]
+	WITH EXECUTE AS OWNER		
+	AFTER INSERT, UPDATE 
+AS
+BEGIN 
+	
+	IF (ROWCOUNT_BIG() =0)
+	RETURN;
+
+	SET NOCOUNT ON;
+	IF EXISTS (
+		SELECT CreatedBy, CreatedOn FROM deleted
+		EXCEPT 
+		SELECT CreatedBy, CreatedOn FROM inserted
+	)
+	BEGIN
+		;THROW 51000, N'Updating columns CreatedBy, CreatedOn is not allowed!', 1;
+	END
+
+	DECLARE @SuserSname nvarchar(128) 
+			,@Datetime DATETIME2(3) = SYSDATETIME()
+
+	EXECUTE AS CALLER 
+		SET @SuserSname = SUSER_SNAME()
+	REVERT;
+
+	--CreateRecord
+	UPDATE u 
+		SET CreatedBy		= @SuserSname 
+		   ,CreatedOn		= @Datetime
+		   ,LastModifiedBy	= @SuserSname
+		   ,LastModifiedOn	= @Datetime
+	FROM inserted i
+	INNER JOIN [conf].[tPermission] u ON i.PermissionId = u.PermissionId
+	LEFT JOIN deleted d				ON i.PermissionId = d.PermissiondId
+	WHERE d.PermissionId IS NULL
+
+
+
+	IF EXISTS (
+		SELECT LastModifiedBy, LastModifiedOn FROM deleted
+		EXCEPT 
+		SELECT LastModifiedBy, LastModifiedOn FROM inserted
+	)
+	AND NOT EXISTS (
+		SELECT UserName, LoginId, DefaultSchema, IsActive FROM deleted
+		EXCEPT 
+		SELECT UserName, LoginId, DefaultSchema, IsActive FROM inserted
+	)
+	BEGIN 
+		UPDATE u 
+			SET LastModifiedBy	= @SuserSname
+		FROM inserted i
+		INNER JOIN [conf].[tPermission] u ON i.PermissionId = u.PermissionId
+		LEFT JOIN deleted d				ON i.PermissionId = d.PermissionId
+		WHERE	i.LastModifiedBy	<> @SuserSname
+
+		UPDATE u 
+			SET LastModifiedOn	= d.LastModifiedOn
+		FROM inserted i
+		INNER JOIN [conf].[tPermission] u ON i.PermissionId = u.PermissionId
+		LEFT JOIN deleted d				ON i.PermissionId = d.PermissionId
+		WHERE	i.LastModifiedBy	= @SuserSname
+			AND i.LastModifiedOn	<> @Datetime
+	END
+
+	IF EXISTS (
+		SELECT * FROM deleted
+		EXCEPT 
+		SELECT * FROM inserted
+	)
+	BEGIN 
+	--UpdateRecord
+	UPDATE u 
+		SET LastModifiedBy	= @SuserSname
+		   ,LastModifiedOn	= @Datetime
+	FROM inserted i
+	INNER JOIN [conf].[tPermission] u ON i.PermissionId = u.PermissionId
+	LEFT JOIN deleted d				ON i.PermissionId = d.PermissionId
+	WHERE	i.LastModifiedBy		<> @SuserSname
+		OR  i.LastModifiedOn		<> @Datetime
+	--	OR  i.[LoginId]				<> d.[LoginId]
+	--  OR  i.[DefaultSchema]	<> d.[DefaultSchema]
+	--	OR	i.[IsActive]			<> d.[IsActive]
+
+	END 
+
+END 
