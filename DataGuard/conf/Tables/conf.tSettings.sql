ff980f4a3d6de82a36b023c3673bc10e7c9e61a9
Damian Domański <d.domanski@dalion.pl>
2024-05-15 00:15:39 +0200

    wip table conf.tSettings

--- DataGuard/conf/Tables/conf.tSettings.sql
+++ DataGuard/conf/Tables/conf.tSettings.sql
@@ -1,6 +1,11 @@
 ﻿CREATE TABLE [conf].[tSettings]
 (
-	 [SettingId] INT NOT NULL PRIMARY KEY
-	,[Setting]	VARCHAR(50) NOT NULL
-	,[Value] VARCHAR(50) NOT NULL
+	 [SettingId]				INT					IDENTITY (1, 1)														NOT NULL,
+	 [SettingName]				VARCHAR(50)																				NOT NULL,
+	 [Value]					VARCHAR(50)																				NOT NULL,
+	 [CreatedBy]				NVARCHAR (128)		CONSTRAINT [conf_dfSettings_CreatedBy]		DEFAULT (SYSTEM_USER)	NOT NULL,
+	 [CreatedOn]			    DATETIME2(3)		CONSTRAINT [conf_dfSettings_CreatedOn]		DEFAULT (GETDATE())		NOT NULL,
+	 [LastModifiedBy]			NVARCHAR (128)		CONSTRAINT [conf_dfSettings_LastModifiedBy]	DEFAULT (SYSTEM_USER)	NOT NULL,
+	 [LastModifiedOn]			DATETIME2(3)		CONSTRAINT [conf_dfSettings_LastModifiedOn]	DEFAULT (GETDATE())		NOT NULL,
+													CONSTRAINT [conf_pkSettings]				PRIMARY KEY CLUSTERED ([SettingId] ASC),
 )
