b05cb6afd10577d744518c795383e34ca906ae94
Damian Domański <d.domanski@dalion.pl>
2024-06-05 00:54:47 +0200

    Add id columns to mon schema tables

--- DataGuard/mon/Tables/mon.tPermissionState.sql
+++ DataGuard/mon/Tables/mon.tPermissionState.sql
@@ -1,12 +1,12 @@
 ﻿CREATE TABLE [mon].[tPermissionState]
 (
---	[Id] INT NOT NULL PRIMARY KEY,
-	[Database]		SYSNAME NULL,
-	[User]			SYSNAME NULL,
-	[Role]			SYSNAME NULL,
-	[ObjectType]	SYSNAME NULL,
-	[Schema]		SYSNAME NULL,
-	[ObjectName]	SYSNAME NULL,
-	[Permission]	SYSNAME NULL
+	[PermissionStateId]		INT		NOT NULL PRIMARY KEY,
+	[Database]				SYSNAME		NULL,
+	[User]					SYSNAME		NULL,
+	[Role]					SYSNAME		NULL,
+	[ObjectType]			SYSNAME		NULL,
+	[Schema]				SYSNAME		NULL,
+	[ObjectName]			SYSNAME		NULL,
+	[Permission]			SYSNAME		NULL
 
 )
--- DataGuard/mon/Tables/mon.tUserState.sql
+++ DataGuard/mon/Tables/mon.tUserState.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [mon].[tUserState]
 (
---	[Id] INT NOT NULL PRIMARY KEY,
-	[DatabaseName]		SYSNAME NULL,
-	[User]				SYSNAME NULL,
-	[PermissionType]	varchar(100) NULL
+	[UserStateId]		INT			NOT NULL PRIMARY KEY,
+	[DatabaseName]		SYSNAME			NULL,
+	[User]				SYSNAME			NULL,
+	[PermissionType]	varchar(100)	NULL
 
 )
